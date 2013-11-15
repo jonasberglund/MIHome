d364abc741ba7a6d9b55e5808bd459bc2fad17ce
Jonas Berglund <berglund.jonas@gmail.com>
2013-11-15 12:42:08 +0100

    first draft

--- MIHome/MIHome/ViewController.h
+++ MIHome/MIHome/ViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <MKMapViewDelegate>
+@property (strong, nonatomic) IBOutlet UILabel *homeLabel;
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
--- MIHome/MIHome/ViewController.m
+++ MIHome/MIHome/ViewController.m
@@ -7,23 +7,69 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController (){
+    CLLocationManager *locationManager;
+    BOOL isHome;
+    CLLocationCoordinate2D coord;
+    long epsilon;
+}
 
 @end
 
 @implementation ViewController
+@synthesize mapView;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.mapView.delegate = self;
+    
+    isHome = false;
+    epsilon = 0.2;
+    
+    //This is home
+    coord.longitude = 11.937437;
+    coord.latitude = 57.709129;
+    
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.distanceFilter = kCLDistanceFilterNone; // whenever we move
+    locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters; // 100 m
+    [locationManager startUpdatingLocation];
+    
+    NSLog(@"%@", [self deviceLocation]);
+    
+    if(fabs(locationManager.location.coordinate.latitude - coord.latitude) <= epsilon &&
+       fabs(locationManager.location.coordinate.longitude - coord.longitude) <= epsilon){
+        _homeLabel.text = @"Yes!";
+    }else{
+        _homeLabel.text = @"Nope!";
+    }
+    
+    NSLog(@"%f", fabs(locationManager.location.coordinate.latitude - coord.latitude));
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSString *)deviceLocation {
+    return [NSString stringWithFormat:@"latitude: %f longitude: %f", locationManager.location.coordinate.latitude, locationManager.location.coordinate.longitude];
 }
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
+    [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
+    
+    MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
+    
+    point.coordinate = coord;
+    point.title = @"This is home!";
+    //point.subtitle = @"I'm here!!!";
+    
+    [self.mapView addAnnotation:point];
+
+}
+
 
 @end
